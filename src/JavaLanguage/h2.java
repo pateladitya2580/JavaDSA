2fc5a119ad38c78c635ea05069ed12d6fde7b608
pateladitya2580 <pateladitya2580@gmail.com>
2026-06-12 21:04:15 +0530

    .

--- src/JavaLanguage/h2.java
+++
@@ -1,7 +0,0 @@
-package JavaLanguage;
-
-public class h2 {
-    static void main(String[] args) {
-        System.out.println("Hello Aditya");
-    }
-}
---
+++ src/JavaLanguage/methods/biodata.java
@@ -0,0 +1,12 @@
+package JavaLanguage.methods;
+
+public class biodata {
+    static void bio(){
+        System.out.println("Aditya Patel");
+        System.out.println("AIML Engineer");
+        System.out.println("Working at Razor Pay as SDE1");
+    }
+    static void main(String[] args) {
+       bio();
+    }
+}
