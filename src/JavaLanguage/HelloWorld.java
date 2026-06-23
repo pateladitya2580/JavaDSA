f5a3924e1dc0ff95d0396203378b8970d320093c
pateladitya2580 <pateladitya2580@gmail.com>
2026-06-23 21:07:17 +0530

    .

--- src/JavaLanguage/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package JavaLanguage;
-
-public class HelloWorld {
-    static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
---
+++ src/JavaLanguage/hellowWorld.java
@@ -0,0 +1,7 @@
+package JavaLanguage;
+
+public class hellowWorld {
+    static void main(String[] args) {
+        System.out.println("Hello Aditya ");
+    }
+}
