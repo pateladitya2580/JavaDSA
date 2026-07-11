be1c62551e88c48cfb0f438a6ae587d6e4d49c92
pateladitya2580 <pateladitya2580@gmail.com>
2026-07-11 11:04:48 +0530

    .

--- src/DSA/array/ArrayList/basicOperationOnArrayList.java
+++ src/DSA/array/ArrayList/basicOperationOnArrayList.java
@@ -1,6 +1,27 @@
 package DSA.array.ArrayList;
+
+import java.util.ArrayList;
+
 //ArrayList is a class in Java that implements a dynamic array. Unlike a regular array,
 //an ArrayList can grow and shrink automatically as elements are added or removed.
 public class basicOperationOnArrayList {
+    static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();// small bracket me capacity de sakte hai
+        arr.add(0,10);
+        arr.add(1,59);
 
+        System.out.println(arr);// puri list print hogi
+
+        System.out.println(arr.get(0));
+
+        arr.set(0,56);
+        System.out.println(arr.get(0));
+
+        System.out.println(arr.size());
+
+        arr.add(89);//push back
+        System.out.println(arr);
+
+        //arr.remove() isme index dalo uska or wo element delete
+      }
 }
