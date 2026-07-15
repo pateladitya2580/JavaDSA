2ecec4dc0dc5eaf2381662ab72bc0d002eea6348
pateladitya2580 <pateladitya2580@gmail.com>
2026-07-15 12:15:33 +0530

    .

---
+++ src/DSA/array/question/question11Merge2SortedArrays.java
@@ -0,0 +1,41 @@
+package DSA.array.question;
+
+import java.util.Scanner;
+
+//Merge two sorted arrays in one single array.
+public class question11Merge2SortedArrays {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int []a = {11,33,42,71};
+        int []b = {26,54,69,81};
+        int []c = new int[a.length+b.length];
+        int i = 0,j=0,k=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<b[j]){
+                c[k] = a[i];
+                i++;k++;
+            }
+            else{
+                c[k] = b[j];
+                j++;
+                k++;;
+            }
+        }
+        if(i == a.length){// npw take elements from b only
+            while(j<b.length){
+                c[k] = b[j];
+                k++;j++;
+            }
+        }
+        else if(j== b.length){// npw take elements from a  only
+            while (i<a.length){
+                c[k] = a[i];
+                k++;i++;
+            }
+        }
+        for(int ele:c){
+            System.out.print(ele+" ");
+        }
+
+    }
+}
---
+++ src/DSA/array/question/question12NextGreaterElement.java
@@ -0,0 +1,32 @@
+package DSA.array.question;
+//Next greatest element.
+//        arr[] = {12,8,41,37,2,49,16,28,21}
+//        ans[] = {49,49,49,49,49,28,28,21,-1}
+public class question12NextGreaterElement {
+    static void main(String[] args) {
+        int []arr = {12,8,41,37,2,49,16,28,21};
+        int n = arr.length;
+        int []brr = new int [n];
+        // method 1 :Brute Force
+//        for(int i = 0;i<n;i++){
+//            int max = -1;
+//            for(int j = i+1;j<nj++){
+//                if(max<arr[j]){
+//                    max = arr[j];
+//                }
+//            }
+//            brr[i]=max;
+//        }
+        //method 2
+
+        brr[n-1] =-1;
+        int nextGI = arr[n-1];
+        for(int i = n-2;i>=0;i--){
+            brr[i]= nextGI;
+            nextGI = Math.max(arr[i],nextGI);
+        }
+        for(int ele:brr){
+            System.out.print(ele+" ");
+        }
+    }
+}
