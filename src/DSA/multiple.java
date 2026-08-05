f12ee731391d9972e6beb170a1237b66d81066c9
pateladitya2580 <pateladitya2580@gmail.com>
2026-08-05 21:07:22 +0530

    .

--- src/DSA/multiple.java
+++
@@ -1,9 +0,0 @@
-package DSA;
-
-public class multiple {
-    static void main(String[] args) {
-        for (int i = 1; i <= 10 ; i++) {
-            System.out.println(8*i);
-        }
-    }
-}
--- src/DSA/sum.java
+++
@@ -1,8 +0,0 @@
-package DSA;
-
-public class sum {
-    static void main(String[] args) {
-        int sum = 10 + 29;
-        System.out.println(sum);
-    }
-}
