e99885340b13ca72a6f04f62c899bcabc39f9889
pateladitya2580 <pateladitya2580@gmail.com>
2026-08-05 20:38:40 +0530

    .

--- src/DSA/HelloWorld.java
+++ src/DSA/HelloWorld.java
@@ -3,5 +3,7 @@ package DSA;
 public class HelloWorld {
     static void main(String[] args) {
         System.out.println("Aditya Patel");
+        System.out.println("AIML Engineer");
+        System.out.println("Future Engineer");
     }
 }
