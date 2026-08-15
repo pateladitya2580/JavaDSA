58ab435cae3bbc45ada550eb4ff2ac5a02b4cdf4
pateladitya2580 <pateladitya2580@gmail.com>
2026-08-15 18:34:39 +0530

    .

---
+++ src/DSA/ht.java
@@ -0,0 +1,16 @@
+package DSA;
+
+public class ht {
+    static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 1; j <= 4-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <=i ; k++) {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
