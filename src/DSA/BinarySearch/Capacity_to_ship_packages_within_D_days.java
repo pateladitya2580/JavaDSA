b338991e798bfac417fba98505c1a8bff0476bf1
pateladitya2580 <pateladitya2580@gmail.com>
2026-08-29 18:18:29 +0530

    .

---
+++ src/DSA/BinarySearch/Capacity_to_ship_packages_within_D_days.java
@@ -0,0 +1,46 @@
+package DSA.BinarySearch;
+// leet code 1011
+public class Capacity_to_ship_packages_within_D_days {
+    public boolean isPossible(int c ,int[]arr,int d){
+        int n = arr.length;
+        int days = 1;
+        int load =0;
+        for(int i = 0 ;i<n;i++){
+            if(load + arr[i] <= c){
+                load += arr[i];
+            }
+            else{
+                load = arr[i];
+                days++;
+            }
+        }
+        if(days>d)return false;
+        else return true;
+    }
+    public int shipWithinDays(int[] arr, int d) {
+        int n = arr.length;
+        int mx = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0 ;i<n ;i++){
+            mx = Math.max(mx,arr[i]);
+            sum += arr[i];
+        }
+        int low = mx;
+        int high = sum;
+        int minC = 0;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(isPossible(mid,arr,d) == true){
+                minC = mid;
+                high = mid -1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return minC;
+    }
+    static void main(String[] args) {
+
+    }
+}
--- src/DSA/BinarySearch/find_the_smallest_Divisor_given_a_Threshold.java
+++ src/DSA/BinarySearch/find_the_smallest_Divisor_given_a_Threshold.java
@@ -1,6 +1,20 @@
 package DSA.BinarySearch;
 //Leet code 1283
 public class find_the_smallest_Divisor_given_a_Threshold {
+    public boolean isPossible(int mid,int []arr,int t){
+        int n = arr.length;
+        int sum = 0;
+        for(int i = 0 ;i<n ;i++){
+            if(arr[i]%mid ==  0) {
+                sum += arr[i]/mid;
+            }
+            else{
+                sum += arr[i]/mid + 1;
+            }
+        }
+        if(sum <= t) return true;
+        else return false;
+    }
     public int smallestDivisor(int[] arr, int t) {
         int n = arr.length;
         int mx = Integer.MIN_VALUE;
@@ -14,21 +28,13 @@ public class find_the_smallest_Divisor_given_a_Threshold {
         int d = 1;
         while(low<=high){
             int mid = low + (high - low)/2;
-            int sum = 0;
-            for(int i = 0 ;i<n ;i++){
-                if(arr[i]%mid ==  0) {
-                    sum += arr[i]/mid;
-                }
-                else{
-                    sum += arr[i]/mid + 1;
-                }
-            }
-            if(sum > t){
-                low = mid +1;
-            }
-            else if (sum <= t){
+
+            if(isPossible(mid,arr,t)== true){
                 d = mid;
                 high = mid -1;
+            }
+            else{
+                low = mid +1;
             }
         }
         return d;
