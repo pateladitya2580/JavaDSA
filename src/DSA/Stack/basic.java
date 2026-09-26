a037b5ce2dc5ca8def8dcf08a4473f73b5ae2f8f
pateladitya2580 <pateladitya2580@gmail.com>
2026-09-26 15:04:12 +0530

    .

--- src/DSA/Stack/basic.java
+++ src/DSA/Stack/basic.java
@@ -1,25 +1,21 @@
 package DSA.Stack;
-import java.util.LinkedList;
 import java.util.Stack;
 public class basic {
     static void main(String[] args) {
-        Stack<Integer> st = new Stack<>();
-        System.out.println(st.isEmpty());// stack follow first in last out
-        st.push(10);
+       Stack<Integer> st = new Stack<>();//LIFO last in first out
+        System.out.println(st.isEmpty());
+        st.push(10);//Stack mein push() hamesha element ko TOP par add karta hai.
         st.push(20);
         st.push(30);
         st.push(40);
         st.push(50);
         System.out.println(st);
-        System.out.println(st.pop());
-        System.out.println(st.peek());
-        System.out.println(st);
-        System.out.println(st.size());
 
-        //print 10
-        while (st.size()>1){
-            st.pop();
-        }
-        System.out.println(st);
+        System.out.println(st.peek());// peek top element deta hai bina remove ke
+
+        System.out.println(st.pop());//pop se top element milta hai but wo remove ho
+        System.out.println(st);//jata hai stack se
+
+        System.out.println("size is "+ st.size());
     }
 }
